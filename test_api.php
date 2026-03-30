b9996d5ddf1d6b0f69842b5dd8b41c9a4cd5389d
Gabriella Borjas <156150258+gabbieborjas@users.noreply.github.com>
2026-03-30 15:09:52 -0400

    Delete test_api.php

--- test_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-echo json_encode([
-    "message" => "Test endpoints are routed through index.php. Use /test/reset, /test/reveal, /test/placeShips, and /test/forceTurn with the header X-Test-Mode: TEST_PASSWORD."
-]);
